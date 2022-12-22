d9ffd010076ffcc9f3b558498bd1e65a5ae1ff76
= <jhon.sangopanta@epn.edu.ec>
2022-12-22 09:37:43 -0500

    Fixed

---
+++ 02Android/app/src/main/java/com/example/a02_android/ACicloVida.kt
@@ -0,0 +1,95 @@
+package com.example.a02_android
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.a02_android.databinding.ActivityAcicloVidaBinding
+
+class ACicloVida : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityAcicloVidaBinding
+    var textoGlobal = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityAcicloVidaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_aciclo_vida)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+    }
+
+    fun mostrarSnackBar(text:String){
+        textoGlobal += text
+        Snackbar.make(findViewById(R.id.cl_ciclo_vida),
+            textoGlobal, Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_aciclo_vida)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mostrarSnackBar("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mostrarSnackBar("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.run{
+            //Guardar las variables
+            //Primitivos
+            putString("textoGuaradado", textoGlobal)
+        }
+
+        super.onSaveInstanceState(outState)
+    }
+
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        val textoRecuperado: String? = savedInstanceState.getString("Texto guardado")
+
+        if(textoRecuperado!=null){
+            mostrarSnackBar(textoRecuperado)
+            textoGlobal=textoRecuperado
+        }
+    }
+}
