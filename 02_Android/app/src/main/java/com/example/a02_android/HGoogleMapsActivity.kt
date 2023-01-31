2536df9b9d5629f424435997f4e39db6fb63d5b6
Fernando Sangopanta <jhon.sangopanta@epn.edu.ec>
2023-01-31 10:51:58 -0500

    Maps activity  contained in empty activity

---
+++ 02_Android/app/src/main/java/com/example/a02_android/HGoogleMapsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a02_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HGoogleMapsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hgoogle_maps2)
+    }
+}
